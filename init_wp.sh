27b471976dbca675d9f853048fe666fe013f5476
nobmizue <nobmizue@google.com>
2017-08-14 15:26:20 +0900

    3rd commit

--- init_wp.sh
+++ init_wp.sh
@@ -5,4 +5,4 @@ user=admin
 password=password
 email=admin@foo.bar
 
-kubectl exec $pod -c web -- bash -c "curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x wp-cli.phar && ./wp-cli.phar core install --url=http://$static_ip --title=gke-test --admin_user=$admin --admin_password=$password --admin_email=$email --path=/var/www/html --allow-root"
+kubectl exec $pod -c web -- bash -c "curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x wp-cli.phar && ./wp-cli.phar core install --url=http://$static_ip --title=gke-test --admin_user=$user --admin_password=$password --admin_email=$email --path=/var/www/html --allow-root"
