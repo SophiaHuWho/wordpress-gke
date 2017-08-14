d1a4a17b035c021075e6e2dbea59bfdca328da31
nobmizue <nobmizue@google.com>
2017-08-14 12:08:37 +0900

    2nd commit

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
+#
+# gcloud config for specific K8S cluster
+#
 gcloud config set compute/zone asia-northeast1-a
 gcloud config set container/cluster cluster-1
 gcloud container clusters get-credentials cluster-1
---
+++ init_wp.sh
@@ -0,0 +1,8 @@
+pod=[POD]
+static_ip=[STATIC_IP_ADDR]
+title="Wordpress Test"
+user=admin
+password=password
+email=admin@foo.bar
+
+kubectl exec $pod -c web -- bash -c "curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x wp-cli.phar && ./wp-cli.phar core install --url=http://$static_ip --title=gke-test --admin_user=$admin --admin_password=$password --admin_email=$email --path=/var/www/html --allow-root"
